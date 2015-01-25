cb7add031f8a3aa487506524c82901a5745fbf65
blair <blair@tentwentyfour.ca>
2015-01-24 20:56:58 -0600

    Added EmailEncodingExtension

---
+++ Tests/Twig/EmailEncodingExtensionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Ten24\UtilitiesBundle\Tests;
+
+class EmailEncodingExtensionTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetName()
+    {
+
+    }
+
+    public function testEncodeText()
+    {
+
+    }
+
+    public function testGetFilters()
+    {
+
+    }
+
+    public function testFilter()
+    {
+
+    }
+}
